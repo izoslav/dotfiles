50d7e0d6168194cb9d214892a36cc5d84bd4f63e
Marcin Kołodziej <dev@izoslav.pl>
2025-01-02 22:43:17 +0100

    add aliases

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -3,12 +3,36 @@ if status is-interactive
 end
 
 # Aliases
+## generic
+alias ..='cd ..'
+alias grep='rg'
+alias mkdir='mkdir -pv'
+
+## git
+alias ga='git add'
+alias gaa='git add .'
+alias gc='git commit'
+alias gca='git commit --amend'
+alias gcm='git commit -m'
+alias gcan='git commit --amend --no-edit'
+alias gfp='git fetch -p'
+alias gp='git pull'
+alias gr='git rebase'
+alias gl='git log'
+alias gb='git branches'
+alias gbr='git branches -r'
+
+## ls
 alias ls='eza'
 alias l='eza -l --all --group-directories-first --git'
 alias ll='eza -l --all --all --group-directories-first --git'
 alias lt='eza -T --git-ignore --level=2 --group-directories-first'
 alias llt='eza -lT --git-ignore --level=2 --group-directories-first'
 alias lT='eza -T --git-ignore --level=4 --group-directories-first'
+
+## ps
+alias psmem="ps auxf | sort -nr -k 4"
+alias psmem10="ps auxf | sort -nr -k 4 | head -10"
 
 # Paths
 ## rust
